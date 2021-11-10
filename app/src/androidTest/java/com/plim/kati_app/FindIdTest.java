7d41e0cc2fba2f20a8971e72e3c12159bb7b28a0
JungSangHyeon <dev.jusa@gmail.com>
2021-11-10 16:48:05 +0900

    Find Id Test Finished

--- app/src/androidTest/java/com/plim/kati_app/FindIdTest.java
+++ app/src/androidTest/java/com/plim/kati_app/FindIdTest.java
@@ -1,5 +1,7 @@
 package com.plim.kati_app;
 
+import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.PerformException;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
@@ -20,6 +22,7 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -28,39 +31,34 @@ public class FindIdTest {
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
 
-    // 로그아웃 상태에서 로그인 창으로.
-    // 아이디 찾기 클릭
-    // 복구 이메일 입력
-    // 아이디 찾기 클릭
-
-    // 로그아웃 상태에서 로그인 창으로.
-    // 비번 찾기 클릭
-    // 이메일 입력
-    // 이메일 발송 하기 클릭
-    // 대기
-    // 임시 비밀번호 발급하였습니다. <- 확인
-
-    // 사람이 메일 확인
-    // 코드 변수에 넣고 로그인 되는지 확인
-
     @Before
     public void init(){
         onView(withId(R.id.action_mykati)).perform(click());
         try {
-            onView(withId(R.id.myKatiFragment_myInfoLayout)).check(matches(isDisplayed()));
-        } catch (AssertionFailedError e) {
             onView(withId(R.id.myKatiFragment_loginButton)).perform(click());
-            onView(withId(R.id.loginActivity_emaiEditText)).perform(typeText("sh199919@gmail.com"), closeSoftKeyboard());
-            onView(withId(R.id.loginActivity_passwordEditText)).perform(typeText("1234567"), closeSoftKeyboard());
-            onView(withId(R.id.loginActivity_loginButton)).perform(click());
+        } catch (PerformException e) {
+            onView(withId(R.id.myKatiFragment_myInfoLayout)).perform(click());
+            onView(withId(R.id.myInfoEditFragment_logOutTextView)).perform(click());
+            onView(withId(android.R.id.button1)).perform(click());
+            onView(withId(R.id.action_mykati)).perform(click());
+            onView(withId(R.id.myKatiFragment_loginButton)).perform(click());
         }
-        onView(withId(R.id.myKatiFragment_myInfoLayout)).perform(click());
     }
 
     @Test
-    public void logOutTest(){
-        onView(withId(R.id.myInfoEditFragment_logOutTextView)).perform(click());
+    public void findId(){
+        onView(withId(R.id.loginActivity_findIdTextView)).perform(click());
+        onView(withId(R.id.idFindActivity_emailEditText)).perform(typeText("sh199919@naver.com"), closeSoftKeyboard());
+        onView(withId(R.id.idFindActivity_sumbitButton)).perform(click());
+        for(int i=0; i<5; i++){
+            try{
+                onView(withText("복구 이메일으로 기존 아이디(이메일)가 발송되었습니다.")).check(matches(isDisplayed()));
+                break;
+            }catch (NoMatchingViewException e){
+                try { Thread.sleep(1000); } catch (InterruptedException ignored) { }
+            }
+        }
         onView(withId(android.R.id.button1)).perform(click());
-        onView(withId(R.id.action_mykati)).check(matches(isDisplayed()));
+        onView(withId(R.id.activity_main)).check(matches(isDisplayed()));
     }
 }
